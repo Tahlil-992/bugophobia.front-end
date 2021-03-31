95adc97765f4a9f7052ca0589f47a1162f25a7bd
ali-fata <alifata313@yahoo.com>
2021-03-31 16:39:21 +0430

    setup completed & resolve minor mistake

--- src/core/LoginSignUp/reducer/LoginSignUp.js
+++ src/core/LoginSignUp/reducer/LoginSignUp.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import {LoginSignUpActionType} from "../action/LoginSignUpActionType"
 const initialstate = { test : "Undefined"}
 const Todo = (state = initialstate, { type, payload }) => {
@@ -15,20 +14,3 @@ const Todo = (state = initialstate, { type, payload }) => {
   }
 }
 export default Todo;
-=======
-import {ActionTypes} from "../action/actionTypes"
-const initialstate = { test : "Undefined"}
-export default (state = initialstate, { type, payload }) => {
-    switch (type) {
-     
-      case ActionTypes.test:
-          return{
-            ...state, 
-            test : "1"
-          };
-        
-    default : return state;
-
-    }
-}
->>>>>>> 121aa22959cb9d0fbcfc042c88de6f596ec52985
