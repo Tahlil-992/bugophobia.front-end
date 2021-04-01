8775d9281a537ee05076e063473ce84678dac166
ali-fata <alifata313@yahoo.com>
2021-04-01 19:19:14 +0430

    sign up form has done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import SignUpForm from "../src/views/pages/SignUpForm";
+
+
 function App() {
-    return (
-        <div className="App">
-            Blank Page
-        </div>
-    );
+  return (
+    <Router>
+      <Switch>
+        {true &&
+          (
+            <>
+              <Route exact path="/sign-up">
+                <SignUpForm/>
+              </Route>
+              <Route exact path="/forgot-password">
+                <h1>Forgot password</h1>
+              </Route>
+              <Route exact path="/">
+                <h1>Login</h1>
+              </Route>
+            </>
+          )}
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
