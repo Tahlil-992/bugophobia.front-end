621792874cb644dac749d5051b1fb74ead216a9e
Amirhossein Roudgar <2000amir1379@gmail.com>
2021-04-04 20:42:14 +0430

    added initial regex handling

--- src/views/pages/SignUpForm.js
+++ src/views/pages/SignUpForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import "../../style.css";
 import User_photo from "../../assets/images/User_photo.png";
 import Password_photo from "../../assets/images/Password_photo.png";
@@ -9,8 +9,11 @@ import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
+import Snackbar from '@material-ui/core/Snackbar';
 import { Link } from "react-router-dom";
+import CloseIcon from '@material-ui/icons/Close';
 import axios from "axios";
+// import ErrorOutlineIcon from '@material-ui/icons/ErrorOutline';
 
 const callSignUPAPI = async ({ username, password, email }) => {
   try {
@@ -34,21 +37,106 @@ const callSignUPAPI = async ({ username, password, email }) => {
       payload: response.data,
     };
   }
-  catch (e){
+  catch (e) {
     const error = e
-    const {status = '', statusText = '', headers = {}, data = null} = error;
+    const { status = '', statusText = '', headers = {}, data = null } = error;
     const result = {
       status,
       statusText,
       headers,
       errorCode: status,
       payload: data,
-    };      
+    };
     throw result;
   }
 }
 
+const emailRegex = RegExp(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/);
+const userNameRegex = RegExp(/^(?!.*\.\.)(?!.*\.$)[^\W][\w.]{0,29}$/);
+const passwordRegex = RegExp(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/);
+
 export default function SignUp() {
+
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [configPass, setConfigPass] = useState("");
+
+  const [isEmailValid, setIsEmailValid] = useState(true);
+  const [isUsernameValid, setIsUsernameValid] = useState(true);
+  const [ispasswordValid, setIsPasswordValid] = useState(true);
+  const [isConfigPassValid, setIsConfigPassValid] = useState(true);
+
+  const [message, setMessage] = useState("Invalid pattern for email!");
+  const [openSnackBar, setOpenSnackBar] = useState(false);
+
+  const checkEmail = () => {
+    const res = emailRegex.test(email)
+    setIsEmailValid(res);
+    if (message !== "" || message !== null || message !== undefined)
+    {
+      if (!res)
+        setMessage("Invalid pattern for email!");
+    }
+  }
+
+  const checkUsername = () => {
+    const res = userNameRegex.test(username);
+    setIsUsernameValid(res);
+    if (message !== "" || message !== null || message !== undefined)
+    {
+      if (!res)
+        setMessage("Invalid pattern for username!");
+    }
+  }
+
+  const checkPassword = () => {
+    const res = passwordRegex.test(password);
+    setIsPasswordValid(res);
+    if (message !== "" || message !== null || message !== undefined)
+    {
+      if (!res)
+        setMessage("Invalid pattern for password!");
+    }
+  }
+
+  const checkConfigPass = () => {
+    const res = (configPass === password);
+    setIsConfigPassValid(res);
+    if (message !== "" || message !== null || message !== undefined)
+    {
+      if (!res)
+        setMessage("Enter the password again!");
+    }
+  }
+
+  const validateInputs = () => {
+    checkEmail();
+    checkUsername();
+    checkPassword();
+    checkConfigPass();
+    if (isEmailValid && isUsernameValid && ispasswordValid && isConfigPassValid)
+      return true;
+    return false;
+  }
+
+  const handleSubmit = () => {
+    const isValid = validateInputs();
+    if (!isValid) {
+      setPassword("");
+      setConfigPass("");
+      setOpenSnackBar(true);
+    }
+    else {
+      console.log("info correct");
+    }
+  }
+
+  const handleClose = () => {
+    setOpenSnackBar(false);
+  }
+  console.log(message);
+
   return (
     <Box>
       <Container component="main" maxWidth="xs">
@@ -66,12 +154,15 @@ export default function SignUp() {
               </Grid>
               <Grid item xs={12}>
                 <TextField
+                  error={!isEmailValid}
                   variant="outlined"
                   required
                   fullWidth
                   id="email"
                   label="Email Address"
                   name="email"
+                  value={email}
+                  onChange={event => setEmail(event.target.value)}
                 />
               </Grid>
               <Grid container spacing={0}>
@@ -84,12 +175,15 @@ export default function SignUp() {
               </Grid>
               <Grid item xs={12}>
                 <TextField
+                  error={!isUsernameValid}
                   variant="outlined"
                   required
                   fullWidth
                   id="username"
                   label="UserName"
                   name="username"
+                  value={username}
+                  onChange={event => setUsername(event.target.value)}
                 />
               </Grid>
               <Grid container spacing={0}>
@@ -102,6 +196,7 @@ export default function SignUp() {
               </Grid>
               <Grid item xs={12}>
                 <TextField
+                  error={!(ispasswordValid && isConfigPassValid)}
                   variant="outlined"
                   required
                   fullWidth
@@ -110,10 +205,13 @@ export default function SignUp() {
                   type="password"
                   id="password"
                   autoComplete="current-password"
+                  value={password}
+                  onChange={event => setPassword(event.target.value)}
                 />
               </Grid>
               <Grid item xs={12}>
                 <TextField
+                  error={!(ispasswordValid && isConfigPassValid)}
                   variant="outlined"
                   required
                   fullWidth
@@ -122,10 +220,12 @@ export default function SignUp() {
                   type="password"
                   id="Rpassword"
                   autoComplete="current-password"
+                  value={configPass}
+                  onChange={event => setConfigPass(event.target.value)}
                 />
               </Grid>
             </Grid>
-            <Button type="submit" variant="contained" class="button">Sign Up</Button>
+            <Button type="submit" variant="contained" class="button" onClick={handleSubmit}>Sign Up</Button>
             <Grid>
               <Grid item>
                 <Link to="/">Already have an account? log in</Link>
@@ -133,6 +233,17 @@ export default function SignUp() {
             </Grid>
           </div>
         </div>
+        <Snackbar
+          anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+          open={openSnackBar}
+          message={<Typography style={{color: "#611a15"}}>{message}</Typography>}
+          ContentProps={{
+            style:{backgroundColor: "#f9a099"}
+          }}
+          autoHideDuration={6000}
+          action={<CloseIcon onClick={handleClose} style={{color: "#611a15"}}/>}
+        >
+        </Snackbar>
       </Container>
     </Box>
   );
