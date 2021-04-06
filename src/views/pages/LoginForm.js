fe46161e1f33c05a955479dc8b44dd48c0eeaec5
MohammadReza Kia <81178700+mo-kia@users.noreply.github.com>
2021-04-07 00:23:08 +0430

    Update LoginForm.js

--- src/views/pages/LoginForm.js
+++ src/views/pages/LoginForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect,useState } from 'react';
 import "../../style.css";
 import User_photo from "../../assets/images/User_photo.png";
 import Password_photo from "../../assets/images/Password_photo.png";
@@ -10,9 +10,137 @@ import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
+import Snackbar from '@material-ui/core/Snackbar';
 import { Link } from "react-router-dom";
+import CloseIcon from '@material-ui/icons/Close';
+import axios from "axios";
+import IconButton from '@material-ui/core/IconButton';
+import ErrorOutlineIcon from '@material-ui/icons/ErrorOutline';
+import { LoadingSpinner } from "../../assets/loading.spinner"
+
+const callLoginAPI = async ({ username, password }) => {
+  try {
+    const response = await axios({
+      url: "/auth/token/",
+      method: "POST",
+      baseURL: "http://localhost:8000",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: {
+        username,
+        password
+      }
+    })
+    return {
+      status: response.status,
+      statusText: response.statusText,
+      headers: response.headers,
+      payload: response.data,
+    };
+  }
+  catch (e) {
+    const error = e
+    const { status = '', statusText = '', headers = {}, data = null } = error;
+    const result = {
+      status,
+      statusText,
+      headers,
+      errorCode: status,
+      payload: data,
+    };
+    throw result;
+  }
+}
+
+const userNameRegex = RegExp(/^(?!.*\.\.)(?!.*\.$)[^\W][\w.]{0,29}$/);
+const passwordRegex = RegExp(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/);
 
 export default function LogIn() {
+	
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+	
+	const [isUsernameValid, setIsUsernameValid] = useState(true);
+	const [ispasswordValid, setIsPasswordValid] = useState(true);
+	
+	const [onSubmit, setOnSubmit] = useState(false);
+	const [isLoading, setIsLoading] = useState(false);
+
+	const [message, setMessage] = useState();
+	const [openSnackBar, setOpenSnackBar] = useState(false);
+	
+	const checkUsername = () => {
+		const res = userNameRegex.test(username);
+		setIsUsernameValid(res);
+		if (message === "" || message === null || message === undefined) {
+		if (!res)
+			setMessage("Invalid username!");
+		}
+	}
+
+	const checkPassword = () => {
+		const res = passwordRegex.test(password);
+		setIsPasswordValid(res);
+		if (message === "" || message === null || message === undefined) {
+		if (!res)
+			setMessage("Invalid password!");
+		}
+	}
+	
+	const validateInputs = () => {
+		checkUsername();
+		checkPassword();
+		if (isUsernameValid && ispasswordValid)
+		  return true;
+		return false;
+	}
+	  
+	const handleSubmit = () => {
+		const res = validateInputs();
+		if (!res) {
+		  setPassword("");
+		  setIsPasswordValid(false);
+		  setOpenSnackBar(true);
+		}
+		else {
+		  setOpenSnackBar(false);
+		}
+		setIsLoading(res);
+		setOnSubmit(res);
+	}
+
+	const handleClose = () => {
+		setOpenSnackBar(false);
+		setMessage("");
+	}
+	  
+	const callAPI = async () => {
+		try {
+		  const response = await callLoginAPI({ username, password });
+		  setIsLoading(false);
+		  if (response.status === 200) {
+			setOpenSnackBar(false);
+			alert("Login Successfully ...")
+		  }
+		}
+		catch {
+		  setIsLoading(false);
+		  setPassword("");
+		  setIsPasswordValid(false);
+		  setOpenSnackBar(true);
+		  setMessage("Something went wrong while trying to login");
+		}
+	}
+
+	useEffect(() => {
+		if (onSubmit) {
+		  callAPI();
+		  setOnSubmit(false);
+		}
+
+	  }, [onSubmit]);	
+	
     return (
         <Box>
             <Container component="main" maxWidth="xs">
@@ -30,6 +158,7 @@ export default function LogIn() {
                             </Grid>
                             <Grid item xs={12}>
                                 <TextField
+									error={!isUsernameValid}
                                     variant="outlined"
                                     required
                                     fullWidth
@@ -37,6 +166,8 @@ export default function LogIn() {
                                     label="UserName"
                                     name="username"
                                     autoFocus
+									value={username}
+									onChange={event => setUsername(event.target.value)}
                                 />
                             </Grid>
                             <Grid container spacing={0}>
@@ -49,6 +180,7 @@ export default function LogIn() {
                             </Grid>
                             <Grid item xs={12}>
                                 <TextField
+									error={!(ispasswordValid)}
                                     variant="outlined"
                                     required
                                     fullWidth
@@ -57,11 +189,16 @@ export default function LogIn() {
                                     type="password"
                                     id="password"
                                     autoComplete="current-password"
+									value={password}
+									onChange={event => setPassword(event.target.value)}
                                 />
                             </Grid>
-                            <FormControlLabel control={<Checkbox value="remember"/>} label="Remember me" />
+                            <FormControlLabel control={<Checkbox value="remember" />} label="Remember me" />
                         </Grid>
-                        <Button type="submit" fullWidth variant="contained" class="button" >Log in</Button>
+							<Box display="flex" justifyContent="space-between">
+								<Button type="submit" variant="contained" class="button" onClick={() => handleSubmit()}>Login</Button>
+								{isLoading && <LoadingSpinner />}
+							</Box>
                         <Grid>
                             <Link to="/forget-password">Forget password?</Link>
                         </Grid>
@@ -70,6 +207,23 @@ export default function LogIn() {
                         </Grid>
                     </div>
                 </div>
+				<Snackbar
+				  anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+				  open={openSnackBar}
+				  message={
+					<Box display="flex" alignItems="center">
+					  <ErrorOutlineIcon style={{ color: "#611a15", marginRight: "0.5em" }} />
+					  <Typography style={{ color: "#611a15" }}>{message}</Typography>
+					  <IconButton anchorOrigin={{ vertical: 'top', horizontal: 'center' }}>
+						<CloseIcon onClick={handleClose} style={{ color: "#611a15" }} />
+					  </IconButton>
+					</Box>}
+				  ContentProps={{ style: { backgroundColor: "#f9a099" } }}
+				  autoHideDuration={6000}
+				  onClose={handleClose}
+				  resumeHideDuration={0}
+				>
+				</Snackbar>
             </Container>
         </Box>
     );
