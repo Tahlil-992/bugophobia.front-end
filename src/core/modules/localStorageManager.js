b2f1e1acd8843bb124ebc54310b7728791cb1395
Amirhossein Roudgar <2000amir1379@gmail.com>
2021-04-12 18:31:50 +0430

    dispatch problems handled and selectors added, token removal after refresh bug solved

--- src/core/modules/localStorageManager.js
+++
@@ -1,9 +0,0 @@
-export const resetLocalStorage = async () => {
-    await localStorage.removeItem("accessToken");
-    await localStorage.removeItem("refreshToken");
-}
-
-export const setLocalStorage = async ({ accessToken, refreshToken }) => {
-    await localStorage.setItem("accessToken", accessToken);
-    await localStorage.setItem("refreshToken", refreshToken);
-}
