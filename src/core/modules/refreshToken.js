8e38a10b83a970573f62e009bb243d39cd35842a
Amirhossein Roudgar <2000amir1379@gmail.com>
2021-04-12 15:38:14 +0430

    implemented callAPIHandler for refresh token

---
+++ src/core/modules/refreshToken.js
@@ -0,0 +1,57 @@
+// to be updated => token header format
+const callAPI = async (request, sendToken = true) => {
+    const accessToken = await localStorage.getItem("accessToken");
+    const authorization = sendToken && { "Authorization": `Bearer ${accessToken}` };
+    try {
+        const response = await axios({
+            url: request.url,
+            method: request.method,
+            baseURL: request.baseURL || "http://localhost:8000",
+            headers: {
+                ...request.headers,
+                ...authorization,
+                "Content-Type": "application/json",
+
+            },
+            data: ["PUT", "POST"].includes(request.method) ? request.data : {},
+            params: ["DELETE", "GET"].includes(request.method) ? request.params : {},
+        })
+        return {
+            status: response.status,
+            statusText: response.statusText,
+            headers: response.headers,
+            payload: response.data,
+        };
+    }
+    catch (e) {
+        const error = e.response
+        const { status = '', statusText = '', headers = {}, data = null } = error;
+        const result = {
+            status,
+            statusText,
+            headers,
+            payload: data,
+        };
+        throw result;
+    }
+}
+
+export const callAPIHandler = (request) => {
+    try {
+        const response = callAPI(request);
+        return response;
+    }
+    catch (e) {
+        if (e.status === 401)
+            try {
+                const refreshRequest = { url: "/auth/token/refresh/", method: "POST" };
+                const refreshResponse = callAPI(refreshRequest, true);
+                const retryResponse = callAPI(request);
+                return retryResponse;
+            }
+            catch (e) {
+                if (e.status === 401)
+                    throw e; 
+            }
+    }
+}
