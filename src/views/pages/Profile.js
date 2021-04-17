2af98f2fe247f86166e4690e93489834aebe66f9
mo-kia <mohammadrezakia1178@gmail.com>
2021-04-18 00:17:44 +0430

    modify UI of profile

--- src/views/pages/Profile.js
+++ src/views/pages/Profile.js
@@ -1,34 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import "../../style.css";
 import Grid from '@material-ui/core/Grid';
-//import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
-import { Avatar, Button, Container, makeStyles, Paper } from '@material-ui/core';
+import { Avatar, Button, Container, makeStyles } from '@material-ui/core';
 import Accordion from '@material-ui/core/Accordion';
 import AccordionSummary from '@material-ui/core/AccordionSummary';
 import AccordionDetails from '@material-ui/core/AccordionDetails';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import TextField from '@material-ui/core/TextField';
 import Box from '@material-ui/core/Box';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Snackbar from '@material-ui/core/Snackbar';
-import { Link, useHistory } from "react-router-dom";
-import CloseIcon from '@material-ui/icons/Close';
-import IconButton from '@material-ui/core/IconButton';
-import ErrorOutlineIcon from '@material-ui/icons/ErrorOutline';
 import InputAdornment from "@material-ui/core/InputAdornment";
 import EmailIcon from '@material-ui/icons/Email';
 import PhoneAndroidIcon from '@material-ui/icons/PhoneAndroid';
 import LockIcon from '@material-ui/icons/Lock';
 import LocalHospitalIcon from '@material-ui/icons/LocalHospital';
-import Visibility from "@material-ui/icons/Visibility";
-import VisibilityOff from "@material-ui/icons/VisibilityOff";
-import { LoadingSpinner } from "../../assets/loading.spinner";
-import { setLocalStorage, setSessionStorage, resetLocalStorage, resetSessionStorage } from "../../core/modules/storageManager";
-import { connect } from "react-redux";
-import { login, rememberMe, setIsDoctor } from "../../core/Authentication/action/authActions";
-import { callAPIHandler } from "../../core/modules/refreshToken";
+import WcIcon from '@material-ui/icons/Wc';
+import ApartmentIcon from '@material-ui/icons/Apartment';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import BuildIcon from '@material-ui/icons/Build';
+import WorkIcon from '@material-ui/icons/Work';
+import AlarmIcon from '@material-ui/icons/Alarm';
+import DoubleArrowIcon from '@material-ui/icons/DoubleArrow';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -37,43 +29,15 @@ const useStyles = makeStyles((theme) => ({
         margin: theme.spacing(1),
       },
     },
-    commentsPaper: {
-        display: 'flex',
-        flexWrap: 'wrap',
-        margin: theme.spacing(1),
-        backgroundColor: '#fcfc3c', 
-        '& > *': {
-          margin: theme.spacing(2),
-          padding: theme.spacing(2),
-        },
-    },
-    detailsPaper: {
-        display: 'flex',
-        flexWrap: 'wrap',
-        backgroundColor: '#fcfc3c',
-        '& > *': {
-          margin: theme.spacing(2),
-        },
-    },
     accordion:{
-        display: 'flex',
-        flexWrap: 'wrap',
-        backgroundColor: '#dcdc20',
+        backgroundColor: '#89dee2',
         '& > *': {
             margin: theme.spacing(1),
         }
     },
-    grid: {
-        display: "flex",
-        flexWrap: 'wrap',
-    },
-    medium: {
-      width: theme.spacing(10),
-      height: theme.spacing(10),
-    },
     large: {
-      width: theme.spacing(20),
-      height: theme.spacing(20),
+      width: theme.spacing(30),
+      height: theme.spacing(30),
       margin: theme.spacing(2),
     },
 }));
@@ -117,101 +81,127 @@ export default function Profile () {
     const [editProfile, setEditProfile] = useState(false);
 
     const fields = isDoctor ?
-                    [['First Name', firstName, firstNameDis, setFirstNameDis, <EmailIcon/>],
-                    ['Last Name', lastName, lastNameDis, setLastNameDis, <EmailIcon/>],
+                    [['First Name', firstName, firstNameDis, setFirstNameDis, <DoubleArrowIcon/>],
+                    ['Last Name', lastName, lastNameDis, setLastNameDis, <DoubleArrowIcon/>],
                     ['Email Address', email, emailDis, setEmailDis, <EmailIcon/>],
-                    ['Username', username, usernameDis, setUsernameDis, <EmailIcon/>],
-                    ['Gender', gender, genderDis, setGenderDis, <EmailIcon/>],
-                    ['Age', age, ageDis, setAgeDis, <EmailIcon/>],
+                    ['Username', username, usernameDis, setUsernameDis, <AccountCircleIcon/>],
+                    ['Gender', gender, genderDis, setGenderDis, <WcIcon/>],
+                    ['Age', age, ageDis, setAgeDis, <AlarmIcon/>],
                     ['Phone Number', phoneNumber, phoneNumberDis, setPhoneNumberDis, <PhoneAndroidIcon/>],
-                    ['City', city, cityDis, setCityDis, <EmailIcon/>],
-                    ['Password', password, passwordDis, setPasswordDis, <LockIcon/>],
+                    ['City', city, cityDis, setCityDis, <ApartmentIcon/>],
+                    //['Password', password, passwordDis, setPasswordDis, <LockIcon/>],
                     ['GMC Number', gmcNumber, gmcNumberDis, setGmcNumberDis, <LocalHospitalIcon/>],
-                    ['Filed of Specialization', specialization, specializationDis, setSpecializationDis, <EmailIcon/>],
-                    ['Work Experiece', experience, experienceDis, setExperienceDis, <EmailIcon/>]
+                    ['Filed of Specialization', specialization, specializationDis, setSpecializationDis, <WorkIcon/>],
+                    ['Work Experiece', experience, experienceDis, setExperienceDis, <BuildIcon/>]
                     ]
 
                    :
 
-                   [['First Name', firstName, firstNameDis, setFirstNameDis, <EmailIcon/>],
-                    ['Last Name', lastName, lastNameDis, setLastNameDis, <EmailIcon/>],
+                   [['First Name', firstName, firstNameDis, setFirstNameDis, <DoubleArrowIcon/>],
+                    ['Last Name', lastName, lastNameDis, setLastNameDis, <DoubleArrowIcon/>],
                     ['Email Address', email, emailDis, setEmailDis, <EmailIcon/>],
-                    ['Username', username, usernameDis, setUsernameDis, <EmailIcon/>],
-                    ['Gender', gender, genderDis, setGenderDis, <EmailIcon/>],
-                    ['Age', age, ageDis, setAgeDis, <EmailIcon/>],
+                    ['Username', username, usernameDis, setUsernameDis, <AccountCircleIcon/>],
+                    ['Gender', gender, genderDis, setGenderDis, <WcIcon/>],
+                    ['Age', age, ageDis, setAgeDis, <AlarmIcon/>],
                     ['Phone Number', phoneNumber, phoneNumberDis, setPhoneNumberDis, <PhoneAndroidIcon/>],
-                    ['City', city, cityDis, setCityDis, <EmailIcon/>],
-                    ['Password', password, passwordDis, setPasswordDis, <LockIcon/>],
-                    ['Insurance Type', insurance, insuranceDis, setInsuranceDis, <EmailIcon/>]
+                    ['City', city, cityDis, setCityDis, <ApartmentIcon/>],
+                    //['Password', password, passwordDis, setPasswordDis, <LockIcon/>],
+                    ['Insurance Type', insurance, insuranceDis, setInsuranceDis, <LocalHospitalIcon/>]
                     ];
 
     return (
         <div class="profilepaper">
-             <div className="profileform">
-                <Grid direction="column" container spacing={3}>
-                    <Grid direction="row" container item spacing={3}>
-                        <Avatar className={classes.large} src={profileImage}></Avatar>
-                        <Grid direction="column" item justify="space-around" alignItems="flex-end">
-                            <br></br>
-                            <br></br>
-                            <br></br>
-                            {isDoctor ? (
-                                <div>
-                                    <h2>{"Dr. " + firstName + " " + lastName}</h2>
-                                    <h3>{"specialized of " + specialization}</h3>
-                                    <h4>*****</h4>
-                                </div>
-                            )
-                            :
-                            (
-                                <div>
-                                    <h2>{firstName + " " + lastName}</h2>
-                                    <h3>{"User"}</h3>
-                                </div>
-                            )}
-                        </Grid>
+            <Grid direction="column" container spacing={3}>
+                <Grid direction="row" container item spacing={3}>
+                    <Avatar className={classes.large} src={profileImage}></Avatar>
+                    <Grid direction="column" item justify="space-around" alignItems="flex-end">
+                        <br></br>
+                        <br></br>
+                        <br></br>
+                        {isDoctor ? (
+                            <div>
+                                <h2>{"Dr. " + firstName + " " + lastName}</h2>
+                                <h3>{"specialized of " + specialization}</h3>
+                                <h4>*****</h4>
+                            </div>
+                        )
+                        :
+                        (
+                            <div>
+                                <h2>{firstName + " " + lastName}</h2>
+                                <h3>{"User"}</h3>
+                            </div>
+                        )}
                     </Grid>
-                    </Grid>
-                    <Box>
-            <Container component="main" maxWidth="xs">
-                
-                    
-                    <Box display="flex" justifyContent="space-between" >
-                        <Grid container spacing={2}>
-                        {fields.map((item) => {
-                                    return(
-                                        <Grid item xs={12}>
-                                            <TextField 
-                                                onMouseEnter={() => item[3](true)}
-                                                onMouseLeave={() => item[3](false)}
-                                                disabled={item[2]}
-                                                variant="outlined"
-                                                
-                                                fullWidth
-                                                //id="email"
-                                                label={item[0]}
-                                                //name="email"
-                                                value={item[1]}
-                                                InputProps={{
-                                                    startAdornment: (<InputAdornment position="start">{item[4]}</InputAdornment>)
-                                                }}
-                                                />
+                </Grid>
+            </Grid>
+            <br/>
+            <Grid container direction="row" spacing={0} className={classes.root}>  
+                <Grid item xs={isDoctor ? 4 : ''}>
+                    <Accordion className={classes.accordion}>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />}
+                            aria-controls="panel1a-content"
+                            id="panel1a-header"
+                            >
+                            <Typography >Details</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <div className="profileform">
+                                <Container component="main" maxWidth="xs">                
+                                    <Box display="flex" justifyContent="space-between" >
+                                        <Grid container spacing={2} alignItems="flex-start">
+                                                {fields.map((item) => {
+                                                    return(
+                                                        <Grid item xs={12}>
+                                                            <TextField 
+                                                                onMouseEnter={() => item[3](true)}
+                                                                onMouseLeave={() => item[3](false)}
+                                                                disabled={item[2]}
+                                                                variant="outlined"
+                                                                fullWidth 
+                                                                label={item[0]}
+                                                                value={item[1]}
+                                                                InputProps={{
+                                                                    startAdornment: (<InputAdornment position="start">{item[4]}</InputAdornment>)
+                                                                }}
+                                                                />
+                                                        </Grid>
+                                                        )
+                                                    })}
                                         </Grid>
-                                        )
-                                    })}
-                        </Grid>
-                        
-                        </Box>
-                        <Box display="flex" justifyContent="space-between">
-                            <Button variant="contained" class="button" >Edit Profile</Button>
-                            
-                        </Box>
-                        
-                    
-            </Container>
-        </Box>
-                
-            </div>
+                                    </Box>
+                                    <Box display="flex" justifyContent="space-between">
+                                        <Button variant="contained" class="button" >Edit Profile</Button>
+                                    </Box>
+                                </Container>
+                            </div>
+                        </AccordionDetails>
+                    </Accordion>
+                </Grid>
+                {isDoctor ?
+                    <Grid item xs={7}>
+                        <Accordion className={classes.accordion}>
+                            <AccordionSummary
+                                expandIcon={<ExpandMoreIcon />}
+                                aria-controls="panel1a-content"
+                                id="panel1a-header"
+                                >
+                                    <Typography >Comments</Typography>
+                            </AccordionSummary>
+                            <AccordionDetails>
+                                <div className="profileform">
+                                    <Container component="main" maxWidth="xs">
+                                        <h3><center>No Comments Here!</center></h3>
+                                    </Container>
+                                </div>
+                            </AccordionDetails>
+                        </Accordion>
+                    </Grid> 
+                    : 
+                    <></>
+                }
+            </Grid> 
         </div>
     );
 }
