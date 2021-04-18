ba09c5b42cf6949c5a90566c170d0211a0b1e694
Amirhossein Roudgar <2000amir1379@gmail.com>
2021-04-18 21:19:01 +0430

    added get_comments_api_call

--- src/views/pages/commentSection/index.js
+++ src/views/pages/commentSection/index.js
@@ -3,20 +3,31 @@ import { CommentFragment } from "./CommentFragment";
 // import Grid from "@material-ui/core/Grid";
 import Box from "@material-ui/core/Box";
 import Container from "@material-ui/core/Container";
+import { callAPIHandler } from "../../../core/modules/refreshToken";
+
+const callGetCommentsAPI = async ({ doctor_username }, isRemembered) => {
+    try {
+        const response = await callAPIHandler({ method: "GET", data: { doctor_username: doctor_username }, url: "/profile/comments" }, true, isRemembered);
+        return response;
+    }
+    catch (e) {
+        throw e;
+    }
+}
 
 export const CommentSection = () => {
     const comments = [
-        {info: {user: "ali", content: "bad doctor", time: "3 days ago"}},
-        {info: {user: "hasan", content: "good doctor", time: "10 months ago"}},
+        { info: { user: "ali", content: "bad doctor", time: "3 days ago" } },
+        { info: { user: "hasan", content: "good doctor", time: "10 months ago" } },
     ]
 
     return (
         <Container>
             <Box>
-                <CommentFragment comments={comments}/>
+                <CommentFragment comments={comments} />
             </Box>
         </Container>
 
-        
+
     );
 }
