b76765cf74f0c60b3889fda19f155ed97636439b
mo-kia <mohammadrezakia1178@gmail.com>
2021-06-15 00:20:09 +0430

    update

--- src/views/pages/DoctorSignUpFragment.js
+++ src/views/pages/DoctorSignUpFragment.js
@@ -1,55 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import "../../style.css";
 import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import { Link, useParams, useHistory } from "react-router-dom";
 import IconButton from '@material-ui/core/IconButton';
-import InputAdornment from "@material-ui/core/InputAdornment";
-import LockIcon from '@material-ui/icons/Lock';
-import Visibility from "@material-ui/icons/Visibility";
-import VisibilityOff from "@material-ui/icons/VisibilityOff";
 import Toolbar from '@material-ui/core/Toolbar';
 import AppBar from '@material-ui/core/AppBar';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
-import EmailIcon from '@material-ui/icons/Email';
-import { callAPIHandler } from "../../core/modules/refreshToken";
-import { LoadingSpinner } from "../../assets/loading.spinner";
 import Paper from '@material-ui/core/Paper';
-import Snackbar from '@material-ui/core/Snackbar';
-import Modal from '@material-ui/core/Modal';
-import CloseIcon from '@material-ui/icons/Close';
-import ErrorOutlineIcon from '@material-ui/icons/ErrorOutline';
-import CheckCircleIcon from '@material-ui/icons/CheckCircle';
 import { makeStyles } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
-    modal: {
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center",
-    },
-    modalPaper: {
-      backgroundColor: '#86E08C',
-      border: '2px solid #000',
-      boxShadow: theme.shadows[5],
-      padding: theme.spacing(2, 4, 3),
-    },
-    Btn: {
-        backgroundColor: '#40bad5',
-        textAlign: 'center',
-        fontSize: '1.05em',
-        borderRadius: '10px',
-        textTransform: 'none',
-        height: '2.5em',
-        '&:hover': {
-            backgroundColor: '#5f939a',
-        },
-        marginTop: "1em",
-    },
     paper: {
         margin: theme.spacing(15, 3.5),
         //display: 'flex',
@@ -92,12 +56,6 @@ export default function DoctorSignUpFragment({FirstName, LastName}) {
                                     Your request has been sent to us.<br />
                                     We will check your Medical License Number and send you an email in less than a week.
                                     </Typography>
-                                    {/* <Typography noWrap align='center' style={{fontSize: 18, margin: '1.5em 0em', textDecorationLine: 'underline'}}>
-                                        {email}
-                                    </Typography> */}
-                                    {/* <Link class="link" to='/' style={{fontFamily: `'Josefin Sans', sans-serif`}}>
-                                        Back to landing page
-                                    </Link> */}
                                 </Grid>
                             </Grid>
                         </div>
